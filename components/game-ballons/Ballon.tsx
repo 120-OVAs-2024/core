129f558235b46fc95f664250b1af7df51f844328
alejandro-programador2 <programador2@bookandbooksdigital.com.co>
2024-07-29 18:36:23 -0500

    refactor(game-ballons): change implementation

--- components/game-ballons/Ballon.tsx
+++ components/game-ballons/Ballon.tsx
@@ -3,45 +3,33 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
 import ballons from './data/ballons';
-import { spaceProp } from './types/types';
+import { BallonProps } from './types/types';
 
 import css from './styles/ballon.module.css';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const defaultResult = (_: spaceProp): void => {};
-export default function Ballon({
+const Ballon: React.FC<BallonProps> = ({
   role = 0,
   letter = 'A',
   index = 'hasdh767803h',
   enable = true,
-  onResult = defaultResult,
+  onResult,
   ...props
-}) {
+}) => {
   const refWord = useRef<HTMLImageElement>(null);
 
   useGSAP(() => {
-    if (refWord.current) {
-      if (!enable) {
-        gsap.to(refWord.current, {
-          ease: 'power3.in',
-          y: 90,
-          opacity: 0,
-          duration: 0.7,
-          onComplete: () => {
-            onResult({ index, letter });
-          }
-        });
-      } else {
-        gsap.to(refWord.current, {
-          y: 0,
-          opacity: 1,
-          duration: 1,
-          onComplete: () => {
-            onResult({ index, letter });
-          }
-        });
+    if (!refWord.current) return;
+
+    const animationProps = enable
+      ? { y: 0, opacity: 1, duration: 1 }
+      : { ease: 'power3.in', y: 90, opacity: 0, duration: 0.7 };
+
+    gsap.to(refWord.current, {
+      ...animationProps,
+      onComplete: () => {
+        if (onResult) onResult({ index, letter });
       }
-    }
+    });
   }, [enable]);
 
   return (
@@ -49,15 +37,16 @@ export default function Ballon({
       {...props}
       className={css.container}
       style={{ animationDelay: `${role * 0.5}s` }}
-      aria-label={'palabra  ' + letter}
+      aria-label={letter}
       disabled={!enable}>
       <div className={css.container__responsive}>
-        <img src={ballons[role % 7]} />
-
+        <img src={ballons[role % 7]} alt="" />
         <div ref={refWord} className={css.letter}>
           <p>{letter}</p>
         </div>
       </div>
     </button>
   );
-}
+};
+
+export default Ballon;
