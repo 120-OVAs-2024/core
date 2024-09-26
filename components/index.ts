456de2c0cc03015cb71e9f970b6d121b5fb63679
Frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2024-09-26 15:56:36 -0500

    feat(components): add new SelectGroupActivity component

--- components/index.ts
+++ components/index.ts
@@ -39,3 +39,4 @@ export * from './game-radio-bag';
 export * from './game-jumpSandwich';
 export * from './game-phraseAndImage';
 export * from './game-lego';
+export * from './select-group-activity'
